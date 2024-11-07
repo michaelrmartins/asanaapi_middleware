a5b0d090cf0de2d3f82e0e7a7ad62307b775cdf8
Mike <michaelmartins.js@hotmail.com>
2024-11-07 00:12:31 -0300

    file add / main function using method then converted to async function

---
+++ services/asana-getWorkspace.js
@@ -0,0 +1,19 @@
+const Asana = require('asana');
+
+let client = Asana.ApiClient.instance;
+let token = client.authentications['token'];
+token.accessToken = process.env.ASANA_TOKEN;
+
+async function getWorkspace(){
+    let workspacesApiInstance = new Asana.WorkspacesApi();
+    let opts = { 
+        'limit': 50, 
+        // 'offset': "", 
+        'opt_fields': "email_domains,is_organization,name,offset,path,uri"
+    };
+    
+    const result = await workspacesApiInstance.getWorkspaces(opts);
+    return result.data 
+};
+
+module.exports = getWorkspace;
