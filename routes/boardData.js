9348168dac609c5ce61d64d20db3ce7644213208
Mike <michaelmartins.js@hotmail.com>
2024-11-07 00:13:23 -0300

    Configured to use service folder

--- routes/boardData.js
+++ routes/boardData.js
@@ -2,8 +2,14 @@
 
 const express = require('express')
 const router = express.Router();
+const asanaServiceBoard = require('../services/asana-getWorkspace')
 
-router.get("/workspace", (req, resp) => resp.status(200).send("workspace"))
+router.get('/workspace', async (req, resp) => {
+    const workspace = await asanaServiceBoard();
+    resp.status(200).send(workspace)
+    console.log("finished")
+})
+
 router.get("/teams", (req,resp) => resp.status(200).send("Teams."))
 router.get("/projects", (req, resp) => resp.status(200).send("Projects."))
 router.get("/sections", (req, resp) => resp.status(200).send("Sections."))
