566921961a8cf90a9dee6fc0c378033711e23d00
Mike <michaelmartins.js@hotmail.com>
2024-11-08 22:14:50 -0300

    add routes projects / Teams / Sections / Tasks

--- routes/boardData.js
+++ routes/boardData.js
@@ -1,18 +1,41 @@
-// Board data File
+// Router - Board data File
 
 const express = require('express')
 const router = express.Router();
-const asanaServiceBoard = require('../services/asana-getWorkspace')
+const ServiceBoardGetWorkspace = require('../services/asana-getWorkspace')
+const ServiceBoardGetProjects = require('../services/asana-getProjects')
+const ServiceBoardGetTeams = require('../services/asana-getTeams')
+const ServiceBoardGetSections = require('../services/asana-getSections')
+const ServiceBoardGeTasks = require('../services/asana-getTasks')
 
 router.get('/workspace', async (req, resp) => {
-    const workspace = await asanaServiceBoard();
+    const workspace = await ServiceBoardGetWorkspace();
     resp.status(200).send(workspace)
     console.log("finished")
-})
+}) // End Workspace
 
-router.get("/teams", (req,resp) => resp.status(200).send("Teams."))
-router.get("/projects", (req, resp) => resp.status(200).send("Projects."))
-router.get("/sections", (req, resp) => resp.status(200).send("Sections."))
-router.get("/tasks", (req, resp) => resp.status(200).send("Tasks."))
+router.get("/teams", async (req, resp) => {
+    const teams = await ServiceBoardGetTeams();
+    resp.status(200).send(teams);
+    console.log("Finished")
+}) //End Teams
+
+router.get("/projects", async (req, resp) =>{
+    const projects = await ServiceBoardGetProjects();
+    resp.status(200).send(projects)
+    console.log("Finished, Projects")
+}) // End Projects
+
+router.get("/sections", async (req, resp) => {
+    const sections = await ServiceBoardGetSections();
+    resp.status(200).send(sections)
+    console.log("Finished, Sections")
+}) // End Sections
+
+router.get("/tasks", async (req, resp) => {
+    const tasks = await ServiceBoardGeTasks();
+    resp.status(200).send(tasks)
+    console.log("Finished, Tasks")
+}) //  end Tasks
 
 module.exports = router;
