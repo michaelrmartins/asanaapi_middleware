bb9bf8968c44e327235cdc2fb4dbcd748eb76627
Mike <michaelmartins.js@hotmail.com>
2024-11-10 18:19:10 -0300

    Add - Express Middelware/Json Enabled

--- app.js
+++ app.js
@@ -2,9 +2,14 @@
 
 const express = require('express')
 const app = express();
-
 const routes = require('./routes')
+const path = require ('path')
 
+// Enables App top understand Json requests
+app.use(express.json());
+
+// Map Assets to use by client
+app.use('/assets', express.static(path.join(__dirname, 'views', 'assets')))
 app.use(routes)
 
 module.exports = app;
