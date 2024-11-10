b2a7687b1459f1a1ca70c58d001943e7266adba6
Mike <michaelmartins.js@hotmail.com>
2024-11-10 18:21:24 -0300

    Add - Parameters received from POST requests

--- routes/boardData.js
+++ routes/boardData.js
@@ -8,32 +8,43 @@ const ServiceBoardGetTeams = require('../services/asana-getTeams')
 const ServiceBoardGetSections = require('../services/asana-getSections')
 const ServiceBoardGeTasks = require('../services/asana-getTasks')
 
-router.get('/workspace', async (req, resp) => {
-    const workspace = await ServiceBoardGetWorkspace();
+router.post('/workspace', async (req, resp) => {
+    const userToken = req.body.userToken;
+    const workspace = await ServiceBoardGetWorkspace(userToken);
     resp.status(200).send(workspace)
-    console.log("finished")
+    console.log("Finished, Workspace")
 }) // End Workspace
 
-router.get("/teams", async (req, resp) => {
-    const teams = await ServiceBoardGetTeams();
+// Need's Token and Workspace Gid
+router.post("/teams", async (req, resp) => {
+    const userToken = req.body.userToken;
+    const workspace_gid = req.body.workspace_gid;
+    const teams = await ServiceBoardGetTeams(userToken, workspace_gid);
     resp.status(200).send(teams);
-    console.log("Finished")
-}) //End Teams
+    console.log("Finished, Teams")
+}) //End Teams.
 
-router.get("/projects", async (req, resp) =>{
-    const projects = await ServiceBoardGetProjects();
+router.post("/projects", async (req, resp) =>{
+    const userToken = req.body.userToken;
+    const workspace_gid = req.body.workspace_gid;
+    const team_gid = req.body.team_gid;
+    const projects = await ServiceBoardGetProjects(userToken, workspace_gid, team_gid);
     resp.status(200).send(projects)
     console.log("Finished, Projects")
 }) // End Projects
 
-router.get("/sections", async (req, resp) => {
-    const sections = await ServiceBoardGetSections();
+router.post("/sections", async (req, resp) => {
+    const userToken = req.body.userToken;
+    const project_gid = req.body.project_gid;
+    const sections = await ServiceBoardGetSections(userToken, project_gid);
     resp.status(200).send(sections)
     console.log("Finished, Sections")
 }) // End Sections
 
-router.get("/tasks", async (req, resp) => {
-    const tasks = await ServiceBoardGeTasks();
+router.post("/tasks", async (req, resp) => {
+    const userToken = req.body.userToken;
+    const section_gid = req.body.section_gid;
+    const tasks = await ServiceBoardGeTasks(userToken, section_gid);
     resp.status(200).send(tasks)
     console.log("Finished, Tasks")
 }) //  end Tasks
