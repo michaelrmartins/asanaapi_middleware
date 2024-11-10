4e908c374eaaa34b4cda97ba81d794c4e2eef6e8
Mike <michaelmartins.js@hotmail.com>
2024-11-10 18:22:30 -0300

    Add - Path lib to handle view files and folders

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@
 
 const express = require('express')
 const router = express.Router();
+const path = require('path')
+// const wellcomePage = require('../views/wellcome.html')
 
 // Importing all Routes
 const routeBoardData = require('./boardData');
@@ -10,6 +12,8 @@ const routeUserData = require('./userData');
 router.use("/board", routeBoardData); 
 router.use("/user", routeUserData)
 
-router.get("/", (req, resp)=> resp.status(200).send("homepage."))
+router.get("/", (req, resp)=> {
+        resp.sendFile(path.join(__dirname, '../views/', 'wellcome.html'))
+});
 
 module.exports = router;
